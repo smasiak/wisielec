5370188e1216cad295bce2eed39cf4d32affd0c2
smasiak <szymon.masiak@interia.pl>
2020-01-24 22:09:31 +0100

    drugi commit

--- test.c
+++ test.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
-int main (){
+char tab1[0];
+char tab[3]={"KOT"};
+int main(){
+   scanf("%s", tab1);
+   if(tab1[0]=tab[0]){
+      printf("K _ _");
+      scanf("%s", tab1);
+      if(tab1[0]=tab[1]){
+         printf("K O _");
+      }
+      else 
+      printf("K _ _");
+      if(tab1[0]=tab[2]){
+         printf("K O T");
+      }
+      else 
+      printf("K _ _");
+   }
+   if(tab1[0]=tab[0]){
+      printf("K _ _");
+    }
 
-char tab[4]={"ball"};
-
-printf("%s", tab);
-
-       return 0;
+   if(tab1[0]=tab[1]){
+      printf("_ O _");
+    }
+    
+   if(tab1[0]=tab[2]){
+      printf("_ _ T");
+    }
+    else{
+      printf("Litera nie została odgadniona.");
+    
+}
+  return 0;
 }
